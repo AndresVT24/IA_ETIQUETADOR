5000026d8067c752c38bf8b05a546e0466aefd57
Emilio Palleschi <emilio@MacBook-Air-di-Emilio.local>
2026-03-18 19:29:46 +0100

    final update

--- KNN.py
+++ KNN.py
@@ -1,4 +1,4 @@
-__authors__ = ['1691333']
+__authors__ = ['1782649', '1745722', '1691333']
 __group__ = '10'
 
 import numpy as np
@@ -91,3 +91,4 @@ class KNN:
 
         self.get_k_neighbours(test_data, k)
         return self.get_class()
+    
